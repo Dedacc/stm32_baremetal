9de9bf69f8853e2eac7d21e72d0a0d5cbe166509
dedacc <shivamgrd116@gmail.com>
2025-09-20 10:48:56 +0530

    gpio_output

---
+++ gpio_output/Debug/Src/main.d
@@ -0,0 +1,17 @@
+Src/main.o: ../Src/main.c \
+ /home/shivam/stm32_baremetal/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ /home/shivam/stm32_baremetal/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h \
+ /home/shivam/STM32CubeF4/Drivers/CMSIS/Include/core_cm4.h \
+ /home/shivam/STM32CubeF4/Drivers/CMSIS/Include/cmsis_version.h \
+ /home/shivam/STM32CubeF4/Drivers/CMSIS/Include/cmsis_compiler.h \
+ /home/shivam/STM32CubeF4/Drivers/CMSIS/Include/cmsis_gcc.h \
+ /home/shivam/STM32CubeF4/Drivers/CMSIS/Include/mpu_armv7.h \
+ /home/shivam/stm32_baremetal/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h
+/home/shivam/stm32_baremetal/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+/home/shivam/stm32_baremetal/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h:
+/home/shivam/STM32CubeF4/Drivers/CMSIS/Include/core_cm4.h:
+/home/shivam/STM32CubeF4/Drivers/CMSIS/Include/cmsis_version.h:
+/home/shivam/STM32CubeF4/Drivers/CMSIS/Include/cmsis_compiler.h:
+/home/shivam/STM32CubeF4/Drivers/CMSIS/Include/cmsis_gcc.h:
+/home/shivam/STM32CubeF4/Drivers/CMSIS/Include/mpu_armv7.h:
+/home/shivam/stm32_baremetal/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
